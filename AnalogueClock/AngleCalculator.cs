cdae386f741a5ecc8a7f0ed694a2bc74200d169c
Marcus Freire <marcusfreiremartins@gmail.com>
2023-06-12 00:15:32 -0300

    Adicionar arquivos de projeto.

---
+++ AnalogueClock/AngleCalculator.cs
@@ -0,0 +1,14 @@
+﻿namespace AnalogueClock
+{
+    public class AngleCalculator
+    {
+        public double CalculateAngle(int hours, int minutes)
+        {
+            double hourAngle = (hours % 12) * 30 + (minutes / 60.0) * 30;
+            double minuteAngle = minutes * 6;
+            double angle = Math.Abs(hourAngle - minuteAngle);
+
+            return Math.Min(angle, 360 - angle);
+        }
+    }
+}
---
+++ AnalogueClock/Program.cs
@@ -0,0 +1,18 @@
+﻿using AnalogueClock;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        AngleCalculator angleCalculator = new AngleCalculator();
+        Console.Write("Enter the hours (0-12): ");
+        int hours = Convert.ToInt32(Console.ReadLine());
+            
+        Console.Write("Enter the minutes (0-59): ");
+        int minutes = Convert.ToInt32(Console.ReadLine());
+
+        double lesserAngle = angleCalculator.CalculateAngle(hours, minutes);
+
+        Console.WriteLine($"The lesser angle between the hour and minute hands is: {lesserAngle} degrees");
+    }
+}
